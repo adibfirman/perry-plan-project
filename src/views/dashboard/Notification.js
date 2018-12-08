d5aae63bcb303558278f6233e2b18e70fc46592d
Adib Firman <dib.firman@gmail.com>
2018-12-08 19:09:33 +0700

    adding firebase cloud function

--- src/views/dashboard/Notification.js
+++ src/views/dashboard/Notification.js
@@ -1,9 +1,41 @@
 import React, { Fragment } from 'react'
+import moment from 'moment';
 
-const Notification = () => (
-  <Fragment>
-    <p>Notification</p>
-  </Fragment>
-)
+const Notification = props => {
+
+  const { notifications } = props
+
+  const ListItem = ({ notif }) => {
+    return (
+      <li>
+        <span className="pink-text">{notif.user}</span>
+        <span> {notif.content}</span>
+        <div className="rey-text note-date">{moment(notif.time.toDate()).fromNow()}</div>
+      </li>
+    )
+  }
+  
+  return (
+    <Fragment>
+      <div className="section">
+        <div className="card z-depth-0">
+          <div className="card-content">
+            <span className="card-title">Notification</span>
+            <ul className="notifications">
+            {notifications && notifications.map(notif => {
+              return (
+                <ListItem
+                  key={notif.id}
+                  notif={notif} />
+              )
+            })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  )
+
+}
 
 export default Notification
--- src/views/dashboard/index.js
+++ src/views/dashboard/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
 import { compose } from 'redux';
@@ -8,36 +8,35 @@ import { WithUserNotLoggedIn } from '../../HOC';
 const Notification = React.lazy(() => import('./Notification'))
 const ProjectList = React.lazy(() => import('../projects/List'))
 
-class Dashboard extends Component {
+const Dashboard = props => {
 
-  render () {
-    const { listProject } = this.props
+  const { listProject, listNotification } = props
 
-    return (
-      <div className="dashboard container">
-        <div className="row">
-          <div className="col s12 m6">
-            <ProjectList projects={listProject} />
-          </div>
-          <div className="col s12 m5 offset-m1">
-            <Notification />
-          </div>
+  return (
+    <div className="dashboard container">
+      <div className="row">
+        <div className="col s12 m6">
+          <ProjectList projects={listProject} />
+        </div>
+        <div className="col s12 m5 offset-m1">
+          <Notification notifications={listNotification} />
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 
 }
 
 const mapStateToProps = state => {
   const { firestore } = state
   return {
-    listProject: firestore.ordered.projects
+    listProject: firestore.ordered.projects,
+    listNotification: firestore.ordered.notifications
   }
 }
 
 export default compose(
   connect(mapStateToProps),
-  firestoreConnect(['projects']),
+  firestoreConnect(['projects', { collection: 'notifications', limit: 3 }]),
   WithUserNotLoggedIn()
 )(Dashboard)
